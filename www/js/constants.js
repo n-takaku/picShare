67bc0e569371c054bca705b19326c28bc8ba8bc1
n-takaku <n.takaku@bee-partners.co.jp>
2017-08-12 23:22:48 +0900

    定数ファイル追加

---
+++ www/js/constants.js
@@ -0,0 +1,29 @@
+/*
+* 各HTML定数ファイル
+* 命名：各HTML名の大文字_数字連番(001〜)
+*/
+
+// index.html定数
+const INDEX_001 = "";
+
+// picDetail.html定数
+const PICDETAIL_001 = "タグ編集";
+const PICDETAIL_002 = "削除";
+const PICDETAIL_003 = "ダウンロード";
+const PICDETAIL_004 = "画像を削除しますか?";
+const PICDETAIL_005 = "OK";
+const PICDETAIL_006 = "Cancel";
+const PICDETAIL_007 = "削除に失敗しました。";
+const PICDETAIL_008 = "ダウンロードに失敗しました。";
+
+// picList.html定数
+const PICLIST_001 = "";
+
+// picSelect.html定数
+const PICSELECT_001 = "";
+
+// tagRegist.html定数
+const TAGREGIST_001 = "";
+
+// tagSelect.html定数
+const TAGSELECT_001 = "";
